ca3d11e468768b439870aedeac5505a3f1639b1c
Jon Alpha <Death77485@aol.com>
2022-03-21 15:03:03 -0500

    Added and styled navbar w javascript functionality.

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-// Array.from(document.querySelectorAll('.navigation-button')).forEach((item) => {
-//     item.onClick = () => {
-//         item.parentElement.parentElement.classList.toggle('change');
-//     }
-// });
+
+// Navbar
+
+// Add event listeners to container
+const container = document.querySelector('.container');
+
+// Open navbar
+document.querySelector('.open-navbar-icon').addEventListener('click', () => {
+    container.classList.add('change');
+});
+
+// Close navbar
+document.querySelector('.close-navbar-icon').addEventListener('click', () => {
+    container.classList.remove('change');
+})
+
+// Add bg colors to nav links
+
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+
+document.querySelectorAll('.nav-link').forEach((item, i) => {
+
+    item.style.cssText = `background-color: ${colors[i]}`;
+
+});
+
+// Tours - Switch front/back tour card
 
 document.querySelectorAll('.navigation-button').forEach((item) => {
     item.addEventListener('click', () => {
         item.parentElement.parentElement.classList.toggle('change');
     });
 });
+
